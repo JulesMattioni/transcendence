6302db97a34cc8c753cf283d9f190e86390d82e2
Youl Simonnet <youl.simonnet@gmail.com>
2026-07-20 16:14:38 +0200

    feat: add websocket endpoint for audit functionality and include in main app

--- realtime/main.py
+++ realtime/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from app.routers import health
+from app.routers import health, audit
 
 app = FastAPI(title="realtime")
 app.include_router(health.router)
+app.include_router(audit.router)
