037206cba84971a58dd83f4e2722eebe3e2cf8be
Youl Simonnet <youl.simonnet@gmail.com>
2026-07-22 15:55:09 +0200

    feat(audit): implement mock organization data for user in WebSocket connection

--- realtime/app/routers/audit.py
+++ realtime/app/routers/audit.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.services.connection_manager import manager
 from app.services.get_current_user import get_current_user
+from app.services.event_dispatcher import dispatcher
 from fastapi import HTTPException
 
 router = APIRouter()
+
+
+def mock_org():
+    return {
+        "user_id": 42,
+        "organisations": [
+            {"org_id": 1, "name": "Belle Organisation", "role": "admin"},
+            {"org_id": 2, "name": "Magnifique Organisation", "role": "reader"},
+        ],
+    }
 
 
 @router.websocket("/audit")
@@ -13,8 +24,20 @@ async def audit(websocket: WebSocket, token: str) -> None:
     except HTTPException:
         await websocket.close(code=1008)
         return
-    print(user, flush=True)
-    await manager.connect(websocket, user["id"])
+    try:
+        user_org = mock_org()  # mock
+    except HTTPException:
+        await websocket.close(code=1008)
+        return
+    await manager.connect(
+        websocket,
+        user["id"],
+        user_org["organisations"],
+        user["first_name"],
+        user["last_name"],
+    )
+
+    # await dispatcher.add_client(user["id"])
     try:
         while True:
             message = await websocket.receive_text()
@@ -22,4 +45,4 @@ async def audit(websocket: WebSocket, token: str) -> None:
     except WebSocketDisconnect:
         pass
     finally:
-        manager.disconnect(websocket)
+        manager.disconnect(user["id"])
