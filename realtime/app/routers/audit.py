ed34cd7dde3bd657df61e31ba0a1728eb9344a74
Youl Simonnet <youl.simonnet@gmail.com>
2026-07-23 14:56:01 +0200

    feat(audit): improve websocket event handling and logging

--- realtime/app/routers/audit.py
+++ realtime/app/routers/audit.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket
 from app.services.connection_manager import manager
 from app.services.get_current_user import get_current_user
 from app.services.event_dispatcher import dispatcher
-from fastapi import HTTPException
-from app.schemas.event_in import EventIn 
+from app.schemas.event_in import EventIn
+from app.schemas.event_type import EventType
+import logging
 
 router = APIRouter()
+
+logger = logging.getLogger(__name__)
 
 
 @router.websocket("/audit")
@@ -21,11 +24,29 @@ async def audit(websocket: WebSocket, token: str) -> None:
         user["first_name"],
         user["last_name"],
     )
+
+    try:
+        await dispatcher.publish_event(
+            EventIn(event_type=EventType.AUTH_LOGIN, user_id=user["id"])
+        )
+    except Exception as e:
+        logger.warning(e)
     try:
         while True:
             message = await websocket.receive_text()
             await websocket.send_text(message)
-    except Exception:
-        pass
+    except Exception as e:
+        logger.warning(e)
     finally:
+        try:
+            await dispatcher.publish_event(
+                EventIn(
+                    event_type=EventType.AUTH_LOGOUT,
+                    user_id=user["id"],
+                    first_name=user["first_name"],
+                    last_name=user["last_name"],
+                )
+            )
+        except Exception as e:
+            logger.warning(e)
         manager.disconnect(user["id"])
