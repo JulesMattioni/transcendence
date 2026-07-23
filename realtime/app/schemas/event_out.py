c099b43685c3755879ae5f43c74b8001dfbf3c80
Youl Simonnet <youl.simonnet@gmail.com>
2026-07-23 14:56:23 +0200

    feat(event): add first name and last name fields to EventIn and initialize them in EventOut

--- realtime/app/schemas/event_out.py
+++ realtime/app/schemas/event_out.py
@@ -5,6 +5,6 @@ from app.schemas.event_in import EventIn
 class EventOut(EventIn):
     event_id: str
     timestamp: datetime
-    first_name: str | None
-    last_name: str | None
-    org_name: str | None
+    first_name: str | None = None
+    last_name: str | None = None
+    org_name: str | None = None
